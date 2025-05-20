c2e9ebdc395adecdd18703fae228179ef7ab99b0
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2025-05-20 16:37:24 -0300

    Query paredão

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,7 @@
 --Listar todos os filmes com nome, gênero e ano de lançamento.
 SELECT nome, genero, ano_lancamento
 FROM filme
+  
 --Mostrar todos os atores que participaram de um determinado filme.
 SELECT a.nome as ator, f.nome
 FROM ator a
@@ -9,6 +10,7 @@ INNER JOIN elenco e
 INNER JOIN filme f
   ON e.id_filme = f.id
 WHERE e.id_filme = '174'
+  
 --Contar quantos filmes cada ator participou.
 --Listar todos os filmes dirigidos por um diretor específico.
 SELECT f.nome AS filme, d.nome
@@ -16,14 +18,17 @@ FROM filme f
 INNER JOIN diretor d
   ON f.id_diretor = d.id
 WHERE f.id_diretor = 'DR216'
+  
 --Mostrar os filmes com duração superior a determinado tempo.
 SELECT f.nome AS filme
 FROM filme f
 WHERE cast(f.tempo as int) >= 200
+  
 --Contar a quantidade de filmes por gênero.
 SELECT genero, count(genero) AS qtde
 FROM filme
 GROUP BY genero
+  
 --Mostrar os roteiristas que escreveram mais de um filme.
 SELECT r.id AS id_roteirista, r.nome AS nome, count(f.id_roteirista) AS qtde_filmes
 FROM roteirista r 
@@ -36,6 +41,14 @@ HAVING count(id_roteirista) > 1
 SELECT nome, sexo, idade
 FROM ator
 WHERE sexo = 'feminino' AND cast(idade as int) < 30 
+
+--Listar os filmes com mais de 5 atores no elenco
+SELECT f.nome AS filme, count(e.id_ator) AS qtde_atores
+FROM filme f
+INNER JOIN elenco e
+  ON f.id = e.id_filme
+GROUP BY f.id
+HAVING count(id_ator) > 5
 --Listar todos os filmes de um determinado gênero lançados em um intervalo de anos.
 SELECT f.nome AS filme
 FROM filme f
