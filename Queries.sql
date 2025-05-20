1b240a3694a95acd1177aaeb74b908f8b3302ba0
Nuno Martins <145809068+nunomgs136@users.noreply.github.com>
2025-05-20 15:58:46 -0300

    Query 10 feita

--- Queries.sql
+++ Queries.sql
@@ -8,3 +8,6 @@
 --Apresentar a quantidade de filmes lançados por ano.
 --Listar os atores do sexo feminino com menos de 30 anos.
 --Listar todos os filmes de um determinado gênero lançados em um intervalo de anos.
+SELECT f.nome AS filme
+FROM filme f
+WHERE cast(f.ano_lancamento as int) <= 2020 AND cast(f.ano_lancamento as int) >= 2010 and f.genero = 'Documentário'
